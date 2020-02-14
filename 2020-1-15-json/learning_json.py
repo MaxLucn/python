f889a3372afee96b83c624740b04b5abbb3e1880
MaxLu <maxlu.com@gmail.com>
2020-02-14 16:30:25 +0800

    数据库

--- 2020-1-15-json/learning_json.py
+++ 2020-1-15-json/learning_json.py
@@ -193,54 +193,89 @@
 #
 # 达特茅斯
 # 确保输入的名称和大小写与上面的完全相同，然后在下面输入place_id和您的Python代码。提示：place_id的前七个字符 是“ ChIJEYl ...”
-import urllib.request, urllib.parse, urllib.error
-import json
-import ssl
+# import urllib.request, urllib.parse, urllib.error
+# import json
+# import ssl
+#
+# api_key = False
+# # If you have a Google Places API key, enter it here
+# # api_key = 'AIzaSy___IDByT70'
+# # https://developers.google.com/maps/documentation/geocoding/intro
+#
+# if api_key is False:
+#     api_key = 42
+#     serviceurl = 'http://py4e-data.dr-chuck.net/json?'
+# else :
+#     serviceurl = 'https://maps.googleapis.com/maps/api/geocode/json?'
+#
+# # Ignore SSL certificate errors
+# ctx = ssl.create_default_context()
+# ctx.check_hostname = False
+# ctx.verify_mode = ssl.CERT_NONE
+#
+# while True:
+#     address = input('Enter location: ')
+#     if len(address) < 1: break
+#
+#     parms = dict()
+#     parms['address'] = address
+#     if api_key is not False: parms['key'] = api_key
+#     url = serviceurl + urllib.parse.urlencode(parms)
+#
+#     print('Retrieving', url)
+#     uh = urllib.request.urlopen(url, context=ctx)
+#     data = uh.read().decode()
+#     print('Retrieved', len(data), 'characters')
+#
+#     try:
+#         js = json.loads(data)
+#     except:
+#         js = None
+#
+#     if not js or 'status' not in js or js['status'] != 'OK':
+#         print('==== Failure To Retrieve ====')
+#         print(data)
+#         continue
+#
+#     print(json.dumps(js, indent=4))
+#
+#     lat = js['results'][0]['geometry']['location']['lat']
+#     lng = js['results'][0]['geometry']['location']['lng']
+#     print('lat', lat, 'lng', lng)
+#     location = js['results'][0]['formatted_address']
+#     print(location)
 
-api_key = False
-# If you have a Google Places API key, enter it here
-# api_key = 'AIzaSy___IDByT70'
-# https://developers.google.com/maps/documentation/geocoding/intro
 
-if api_key is False:
-    api_key = 42
-    serviceurl = 'http://py4e-data.dr-chuck.net/json?'
-else :
-    serviceurl = 'https://maps.googleapis.com/maps/api/geocode/json?'
+# import urllib.request, urllib.parse, urllib.error
+# import json
+#
+# base_url = 'http://py4e-data.dr-chuck.net/json?'
+# params = dict()
+# params['address'] = 'Dartmouth'
+# params['key'] = 42
+# url = base_url + urllib.parse.urlencode(params)
+# res = None
+#
+# while True:
+#     try:
+#         res = urllib.request.urlopen(url)
+#     except:
+#         print("request fail.")
+#         quit()
+#
+#     data = res.read().decode()
+#     json_data = json.loads(data)
+#
+#     if json_data and 'status' in json_data and json_data['status'] == 'OK':
+#         print(json_data['results'][0]['place_id'])
+#         quit()
+#     else:
+#         print('request success, check data fail.')
+#         continue
 
-# Ignore SSL certificate errors
-ctx = ssl.create_default_context()
-ctx.check_hostname = False
-ctx.verify_mode = ssl.CERT_NONE
 
-while True:
-    address = input('Enter location: ')
-    if len(address) < 1: break
 
-    parms = dict()
-    parms['address'] = address
-    if api_key is not False: parms['key'] = api_key
-    url = serviceurl + urllib.parse.urlencode(parms)
+# SQL有四个基本基本功能，称之为CRUD，分别是：：创建，读取，更新，删除、
 
-    print('Retrieving', url)
-    uh = urllib.request.urlopen(url, context=ctx)
-    data = uh.read().decode()
-    print('Retrieved', len(data), 'characters')
 
-    try:
-        js = json.loads(data)
-    except:
-        js = None
 
-    if not js or 'status' not in js or js['status'] != 'OK':
-        print('==== Failure To Retrieve ====')
-        print(data)
-        continue
-
-    print(json.dumps(js, indent=4))
-
-    lat = js['results'][0]['geometry']['location']['lat']
-    lng = js['results'][0]['geometry']['location']['lng']
-    print('lat', lat, 'lng', lng)
-    location = js['results'][0]['formatted_address']
-    print(location)
