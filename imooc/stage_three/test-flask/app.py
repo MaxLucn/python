82cf5d4a916a4089eab41b52d51a9ff25031483a
MaxLu <maxlu.com@gmail.com>
2021-01-31 23:48:26 +0800

    imooc 入门 flask 微框架

--- imooc/stage_three/test-flask/app.py
+++ imooc/stage_three/test-flask/app.py
@@ -1,12 +1,22 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
+# 装饰器： 表示一个路由配置：用户在浏览器输入 URL ，使用对应的函数处理其中的业务逻辑（可有多个）
 @app.route('/')
 def hello_world():
-    return 'hello world'
+    return 'hello world,success'
 
 
-if __name__ == '__main__':
-    app.run()
+@app.route('/hello')
+def hello():
+    user = {
+        'name': 'Tom'
+    }
+    return render_template('hello.html', user=user)
+
+
+# 不推荐的启动写法
+# if __name__ == '__main__':
+#     app.run()
