a60813df689850b7fc40f33fbe7fe7c058179ff3
MaxLu <maxlu.com@gmail.com>
2021-02-23 19:44:24 +0800

    imooc 阶段三
    响应上下文

--- imooc/stage_three/my_flask/app.py
+++ imooc/stage_three/my_flask/app.py
@@ -1,11 +1,19 @@
-from flask import Flask, render_template
+from flask import Flask, current_app, render_template, request
 
 app = Flask(__name__)
+
+
+@app.route('/index')
+def index():
+    print(app)
+    print(current_app)
+    return 'index'
 
 
 # 装饰器： 表示一个路由配置：用户在浏览器输入 URL ，使用对应的函数处理其中的业务逻辑（可有多个）
 @app.route('/')
 def hello_world():
+    """视图函数"""
     return 'hello world,success'
 
 
@@ -23,8 +31,11 @@ def list_user(page=1):
     return '您好，您是第{}页用户'.format(page)
 
 
-app.add_url_rule('/home', 'home', hello_world)
-
-# 不推荐的启动写法
-# if __name__ == '__main__':
-#     app.run()
+@app.route('/test/req')
+def test_request():
+    """请求报文练习，获取 get 参数"""
+    get_args = request.args
+    print(get_args)
+    page = request.args.get('page')
+    print(page)
+    return 'request success'
