47b9d3a2fe624cf67825f533d001f8edf271ad65
MaxLu <maxlu.com@gmail.com>
2021-02-24 10:09:37 +0800

    imooc 阶段三
    请求、响应

--- imooc/stage_three/my_flask/app.py
+++ imooc/stage_three/my_flask/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, current_app, render_template, request
+from flask import Flask, current_app, render_template, request, make_response
 
 app = Flask(__name__)
 
@@ -38,4 +38,51 @@ def test_request():
     print(get_args)
     page = request.args.get('page')
     print(page)
+    """服务器所在的主机地址"""
+    headers = request.headers
+    print(headers)
+    print(headers.get('host'))
+    """获取 IP 地址"""
+    ip = request.remote_addr
+    print("远程 IP地址")
+    print(ip)
+
     return 'request success'
+
+
+@app.before_first_request
+def first_request():
+    """服务器启动后第一个请求到达"""
+    print('first_request')
+
+
+@app.before_request
+def per_request():
+    """每一个请求到达前"""
+    print('before request')
+
+
+@app.route('/test/response')
+def test_response():
+    """ 测试响应 """
+    # return 'response success', 201, {
+    #     'user_id': 'my_id'
+    # }
+
+    # 通过 make_response 构造一个响应对象
+    # resp = make_response('这是一个响应对象', 403, {
+    #     'token': '123'
+    # })
+    # resp.headers['user_id'] = 'id_123'
+
+    # 响应 HTML
+    html = "<html><body><h1 style='color:red'>HTML文本显示<h1><body><html>"
+    resp = make_response(html)
+    return resp
+
+
+@app.route('/test/html')
+def test_html():
+    """ 响应 HTML 文件 """
+    html = render_template('index.html')
+    return html
