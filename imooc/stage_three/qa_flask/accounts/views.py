a0c3f3204433665fdd79b2645157274000c78e2d
MaxLu <maxlu.com@gmail.com>
2021-04-04 00:02:40 +0800

    imooc 阶段三 flask实战：在线问答系统的开发
    1、数据库模型代码
    2、使用蓝图改进项目

---
+++ imooc/stage_three/qa_flask/accounts/views.py
@@ -0,0 +1,23 @@
+from flask import Blueprint, render_template
+
+accounts = Blueprint('accounts', __name__,
+                     template_folder='templates',
+                     static_folder='../assets')
+
+
+@accounts.route('/login')
+def login():
+    """ 登陆 """
+    return render_template('login.html')
+
+
+@accounts.route('/register')
+def register():
+    """ 注册 """
+    return render_template('register.html')
+
+
+@accounts.route('/mine')
+def mine():
+    """ 个人中心 """
+    return render_template('mine.html')
---
+++ imooc/stage_three/qa_flask/conf.py
@@ -0,0 +1,12 @@
+import os
+
+
+class Config(object):
+    """ 项目的配置文件 """
+    # 数据库连接 URI
+    SQLALCHEMY_DATABASE_URI = 'mysql://root:123456@localhost/flask_qa'
+    SQLALCHEMY_TRACK_MODIFICATIONS = True
+    # flash, from wtf
+    SECRET_KEY = 'abcdsacb12312'
+    # 文件上传的根路径
+    MEDIA_ROOT = os.path.join(os.path.dirname(__file__), 'medias')
