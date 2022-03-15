e645510cd5d27a3e5244044973cb0705e49a1ad0
MaxLu <57387065+MaxLucn@users.noreply.github.com>
2022-03-15 09:11:33 +0800

    imooc 阶段四 幕旅游网开发
    幕旅游网下单支付接口开发与联调：
    1、订单详情接口
    2、门票支付接口

--- imooc/stage_four/trip/order/urls.py
+++ imooc/stage_four/trip/order/urls.py
@@ -5,4 +5,6 @@ from order import views
 urlpatterns = [
     # 3.1 订单提交接口
     path('ticket/submit/', views.TicketOrderSubmitView.as_view(), name="ticket_submit"),
+    # 3.2 订单详情
+    path('order/detail/<int:sn>/', views.OrderDetail.as_view(), name="order_detail"),
 ]
--- imooc/stage_four/trip/order/views.py
+++ imooc/stage_four/trip/order/views.py
@@ -1,11 +1,16 @@
 import json
 
 from django import http
+from django.db import transaction
 from django.shortcuts import render
 from django.utils.decorators import method_decorator
 from django.views.generic import FormView
+from django.views.generic.detail import BaseDetailView
 
+from order import serializers
+from order.choices import OrderStatus
 from order.forms import SubmitTicketOrderForm
+from order.models import Order
 from utils.response import BadRequestJsonResponse
 from utils.views import login_required
 
@@ -36,3 +41,42 @@ class TicketOrderSubmitView(FormView):
         return http.JsonResponse({
             'sn': obj.sn
         }, status=201)
+
+
+@method_decorator(login_required, name='dispatch')
+class OrderDetail(BaseDetailView):
+    slug_field = 'sn'
+    slug_url_kwarg = 'sn'
+
+    def get_queryset(self):
+        user = self.request.user
+        return Order.objects.filter(user=user, is_valid=True)
+
+    def get(self, request, *args, **kwargs):
+        """ GET: 订单详情 """
+        order_obj = self.get_object()
+        data = serializers.OrderDetailSerializer(order_obj).to_dict()
+        return http.JsonResponse(data)
+
+    @transaction.atomic
+    def post(self, request, *args, **kwargs):
+        """ POST: 订单支付 """
+        # 1. 选择支付方式：支付宝、微信
+        # 2. 数据验证
+        order_obj = self.get_object()
+        if order_obj.status == OrderStatus.SUBMIT:
+            # 3. 调用真实的支付（暂时省略）
+            # 4. 改变订单状态
+            order_obj.status = OrderStatus.PAID
+            order_obj.save()
+            order_obj.order_items.update(status=OrderStatus.PAID)
+            return http.HttpResponse('', status=201)
+        return http.HttpResponse('', status=200)
+
+    def delete(self, request, *args, **kwargs):
+        """ DELETE: 订单删除 """
+        return http.JsonResponse({'result': 'delete'})
+
+    def put(self, request, *args, **kwargs):
+        """ PUT： 取消订单 """
+        return http.JsonResponse({'result': 'put'})
