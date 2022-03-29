0b1e8f3c45368b064e9083575de52dde0e5ad454
MaxLu <57387065+MaxLucn@users.noreply.github.com>
2022-03-29 23:04:27 +0800

    imooc 阶段四 慕旅游网开发
    慕旅游网后台管理系统：
    订单增长统计图的实现、实现统计图所需要的各种依赖

--- imooc/stage_four/trip/master/urls.py
+++ imooc/stage_four/trip/master/urls.py
@@ -3,6 +3,8 @@ from django.urls import path
 from master import views
 
 urlpatterns = [
+    # 统计报表
+    path('', views.index, name='index'),
     # echarts 的使用
     path('test/', views.test, name='test')
 ]
