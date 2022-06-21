4310160144e440384e05359fbc127ec56b84ea55
MaxLu <57387065+MaxLucn@users.noreply.github.com>
2022-06-21 22:38:44 +0800

    尝试写的第一个项目（个人博客）
    1、对之前有问题的地方进行修改
    2、数据迁移
    3、创建超级管理员、添加测试数据

--- imooc/max_blog/blog/admin.py
+++ imooc/max_blog/blog/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import BlogTypes, Blog
+
+
+# 自定义 BlogTypesAdmin、BlogAdmin
+@admin.register(BlogTypes)
+class BlogTypesAdmin(admin.ModelAdmin):
+    list_display = ('id', 'type_name')
+
+
+@admin.register(Blog)
+class BlogAdmin(admin.ModelAdmin):
+    list_display = ('title', 'blog_types', 'images', 'content', 'author', 'created_at', 'update_at')
--- imooc/max_blog/blog/models.py
+++ imooc/max_blog/blog/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+# 创建超级管理员模块
 from django.contrib.auth.models import User
 
 
@@ -6,14 +7,23 @@ class BlogTypes(models.Model):
     """ 博客分类 """
     type_name = models.CharField('博客分类名称', max_length=64)
 
+    def __str__(self):
+        """ 在后台管理界面显示创建好的 type_name """
+        return self.type_name
+
 
 class Blog(models.Model):
     """ 博客模型 """
     title = models.CharField('文章标题', max_length=64)
     content = models.TextField('文章内容')
-    images = models.ImageField('图片', upload_to='%Y%m/file/', max_length=256)
-    author = models.ForeignKey(User, on_delete=models.DO_NOTHING())
-    blog_types = models.ForeignKey(BlogTypes, on_delete=models.DO_NOTHING())
+    images = models.ImageField('图片', upload_to='%Y%m/file/', max_length=256, blank=True)
+    # 创建超级管理员
+    author = models.ForeignKey(User, on_delete=models.DO_NOTHING)
+    # 关联到 BlogTypes 类的字段
+    blog_types = models.ForeignKey(BlogTypes, on_delete=models.DO_NOTHING)
 
     created_at = models.DateTimeField('文章创建时间', auto_now_add=True)
     update_at = models.DateTimeField('文章修改时间', auto_now=True)
+
+    def __str__(self):
+        return "<Blog: %s>" % self.title
