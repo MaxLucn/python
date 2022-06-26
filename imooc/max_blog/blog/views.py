bb5982deee9e1ae7cfab1d1fc196ba0a7b8ba61d
MaxLu <57387065+MaxLucn@users.noreply.github.com>
2022-06-26 22:49:47 +0800

    尝试写的第一个项目(个人博客)
    1、把博客内容返回到页面上
    2、遇到的问题：  render 函数的位置参数的问题

--- imooc/max_blog/blog/views.py
+++ imooc/max_blog/blog/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
-# Create your views here.
+from .models import Blog
+
+
+def blog_list(request):
+    context = ['blogs']
+    # 请求所有的博客，返回到前端页面
+    context[] = Blog.objects.all()
+    return render('blog_list.html', context)
+
+
+def blog_detail(request, blog_pk):
+    context = ['blog']
+    # 请求对应的博客内容
+    context[] = get_object_or_404(Blog, pk=blog_pk)
+    return render('blog_detail.html', context)
