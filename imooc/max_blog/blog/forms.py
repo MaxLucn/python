7dcef090097215e3a61f1ad0f0f05d3fe5c563e7
MaxLu <57387065+MaxLucn@users.noreply.github.com>
2022-07-05 22:35:22 +0800

    尝试写的第一个项目(个人博客)
    1、完成博客列表页面与博客详情页面，博客列表页面与添加博客页面之间的跳转
    2、完成 BootStrap 样式的引入

--- imooc/max_blog/blog/forms.py
+++ imooc/max_blog/blog/forms.py
@@ -5,10 +5,10 @@ from .models import Blog
 class BlogForm(forms.ModelForm):
     class Meta:
         model = Blog
-        fields = (' title', 'title_tag', 'content', 'author', 'category_id')
+        fields = ('id', 'title', 'title_tag', 'content', 'author', 'category_id')
 
         widgets = {
-            # 'id': forms.Select(attrs={'class': 'form-control'}),
+            'id': forms.TextInput(attrs={'class': 'form-control'}),
             'title': forms.TextInput(attrs={'class': 'form-control'}),
             'title_tag': forms.TextInput(attrs={'class': 'form-control'}),
             'content': forms.Textarea(attrs={'class': 'form-control'}),
