f30eb4eabe105206e86a187b89595258cee098b3
MaxLu <57387065+MaxLucn@users.noreply.github.com>
2022-08-11 00:25:09 +0800

    尝试写的第一个项目(个人博客)
    在注册页面添加字段

---
+++ imooc/max_blog/members/forms.py
@@ -0,0 +1,20 @@
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+from django import forms
+
+
+class SignUpForm(UserCreationForm):
+    """ 在注册表单中添加字段 """
+    email = forms.EmailField(widget=forms.EmailInput(attrs={'class': 'form-control'}))
+
+    class Meta:
+        model = User
+        fields = ('username', 'email', 'password1', 'password2')
+
+    def __init__(self, *args, **kwargs):
+        """ 优化注册界面 """
+        super(SignUpForm, self).__init__(*args, **kwargs)
+
+        self.fields['username'].widget.attrs['class'] = 'form-control'
+        self.fields['password1'].widget.attrs['class'] = 'form-control'
+        self.fields['password2'].widget.attrs['class'] = 'form-control'
