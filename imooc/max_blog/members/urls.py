ef3abc093de14aeee4a1cb42c44bc5bb8bd6c805
MaxLu <57387065+MaxLucn@users.noreply.github.com>
2022-08-15 21:51:55 +0800

    尝试写的第一个项目(个人博客)
    1、创建编辑个人信息页面
    2、使用 Bootstrap 优化个人信息界面

--- imooc/max_blog/members/urls.py
+++ imooc/max_blog/members/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     # 注册
     path('register/', views.UserRegisterView.as_view(), name='register'),
+    # 编辑个人信息
+    path('edit_profile/', views.UserEditView.as_view(), name='edit_profile'),
 ]
--- imooc/max_blog/members/views.py
+++ imooc/max_blog/members/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render
 from django.views import generic
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, UserChangeForm
 from django.urls import reverse_lazy
-from members.forms import SignUpForm
+from members.forms import SignUpForm, EditProfileForm
 
 
 class UserRegisterView(generic.CreateView):
@@ -10,3 +10,13 @@ class UserRegisterView(generic.CreateView):
     form_class = SignUpForm
     template_name = 'registration/register.html'
     success_url = reverse_lazy('login')
+
+
+class UserEditView(generic.UpdateView):
+    """ 编辑个人信息 """
+    form_class = EditProfileForm
+    template_name = 'registration/edit_profile.html'
+    success_url = reverse_lazy('blog')
+
+    def get_object(self, queryset=None):
+        return self.request.user
