af3947f1fe7bd5e54c3482e69c0d6edbd85b0103
MaxLu <57387065+MaxLucn@users.noreply.github.com>
2022-08-16 23:23:51 +0800

    尝试写的第一个项目(个人博客)
    完善用户修改密码界面

--- imooc/max_blog/members/urls.py
+++ imooc/max_blog/members/urls.py
@@ -6,4 +6,8 @@ urlpatterns = [
     path('register/', views.UserRegisterView.as_view(), name='register'),
     # 编辑个人信息
     path('edit_profile/', views.UserEditView.as_view(), name='edit_profile'),
+    # 更改用户密码
+    path('password/', views.PasswordsChangeView.as_view()),
+    # 密码修改成功跳转到这里
+    path('password_success', views.password_success, name='password_success')
 ]
--- imooc/max_blog/members/views.py
+++ imooc/max_blog/members/views.py
@@ -1,8 +1,9 @@
+from django.contrib.auth.views import PasswordChangeView
 from django.shortcuts import render
 from django.views import generic
-from django.contrib.auth.forms import UserCreationForm, UserChangeForm
+from django.contrib.auth.forms import UserCreationForm, UserChangeForm, PasswordChangeForm
 from django.urls import reverse_lazy
-from members.forms import SignUpForm, EditProfileForm
+from members.forms import SignUpForm, EditProfileForm, PasswordChangingForm
 
 
 class UserRegisterView(generic.CreateView):
@@ -20,3 +21,18 @@ class UserEditView(generic.UpdateView):
 
     def get_object(self, queryset=None):
         return self.request.user
+
+
+class PasswordsChangeView(PasswordChangeView):
+    """ 修改密码 """
+    form_class = PasswordChangingForm
+    template_name = 'registration/change-password.html'
+    success_url = reverse_lazy('password_success')
+    # success_url = reverse_lazy('blog')
+
+
+def password_success(request):
+    """ 密码修改成功跳转到这里 """
+    return render(request, 'registration/password_success.html', {})
+
+
